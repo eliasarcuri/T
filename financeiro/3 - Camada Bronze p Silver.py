2637579e5c15b77ab67e8431e1108566a77b0022
elias.arcuri.eng@gmail.com <elias.arcuri.eng@gmail.com>
2024-03-20 18:08:13 +0000

    Atualização final
    Atualização final

--- financeiro/3 - Camada Bronze p Silver.py
+++ financeiro/3 - Camada Bronze p Silver.py
@@ -1,6 +1,9 @@
 # Databricks notebook source
 # DBTITLE 1,Conferindo arquivos da camada Bronze
-# MAGIC %fs ls dbfs:/mnt/financeiro/bronze/
+files = dbutils.fs.ls("dbfs:/mnt/financeiro/bronze/")
+sorted_files = sorted(files, key=lambda file: file.modificationTime, reverse=True)
+display(sorted_files)
+
 
 # COMMAND ----------
 
@@ -10,10 +13,12 @@ arquivos = dbutils.fs.ls("/mnt/financeiro/bronze/")
 
 # Filtrar apenas arquivos Parquet
 arquivos_parquet = [arquivo for arquivo in arquivos if arquivo.name.endswith(".parquet")]
+
 # Encontrar o arquivo mais recente com base no timestamp de modificação
 arquivo_mais_recente = max(arquivos_parquet, key=lambda x: x.modificationTime)
- 
-caminho_arquivo_mais_recente = arquivo_mais_recente.path # Caminho completo do arquivo mais recente
+
+# Caminho completo do arquivo mais recente
+caminho_arquivo_mais_recente = arquivo_mais_recente.path 
 df_modelo2 = spark.read.parquet(caminho_arquivo_mais_recente) #df dados novos
 
 display(df_modelo2)
@@ -21,6 +26,7 @@ display(df_modelo2)
 # COMMAND ----------
 
 # DBTITLE 1,Print (visualização das colunas para seleção)
+#Buscando o nome das colunas para inserir no tratamento dos nulos
 print(df_modelo2.columns)
 
 # COMMAND ----------
@@ -40,8 +46,21 @@ df_modelo2.printSchema()
 
 # COMMAND ----------
 
+# DBTITLE 1,Convertendo tipo de dado string para decimal
+from pyspark.sql.functions import col
+from pyspark.sql.types import DecimalType
+
+colunasdec = ['VALOR_PARCELA', 'VALOR_PARCELA_RECEBIDA']
+
+for coluna in colunasdec:
+    df_modelo2 = df_modelo2\
+        .withColumn(coluna, col(coluna).cast(DecimalType(10, 2)))
+
+
+# COMMAND ----------
+
 # DBTITLE 1,Convertendo tipo de dados string para int
-colunasint = ['NUMERO_NF', 'VALOR_PARCELA_RECEBIDA','NUM_PARCELA']
+colunasint = ['NUMERO_NF','NUM_PARCELA']
 
 for csi in colunasint:
     df_modelo2 = df_modelo2\
@@ -71,10 +90,11 @@ df_modelo2.printSchema()
 
 # COMMAND ----------
 
-# DBTITLE 1,Insert no dataframe
-#tabela completa sem os dados novos
+# DBTITLE 1,Insert no dataframe df_novo
+#Dados que já estão armazenados na Silver
 df_modelo1 = spark.read.parquet("dbfs:/mnt/financeiro/Silver/recebimento_silver.parquet/")
 
+#Dados novos (df_modelo2) + Dados já armazenados (df_modelo1)
 df_novo = df_modelo1.union(df_modelo2).orderBy('DataCarga')
 
 
@@ -91,6 +111,7 @@ display(df_novo)
 # COMMAND ----------
 
 # DBTITLE 1,Insert na camada Silver
+#Subscrevendo o arquivo na camada Silver
 df_novo.write.mode("overwrite").parquet("dbfs:/mnt/financeiro/Silver/recebimento_silver.parquet")
 
 # COMMAND ----------
@@ -101,4 +122,7 @@ df_novo.write.mode("overwrite").parquet("dbfs:/mnt/financeiro/Silver/recebimento
 # COMMAND ----------
 
 # DBTITLE 1,Lendo Informações atualizadas na Silver
-display(spark.read.parquet("dbfs:/mnt/financeiro/Silver/recebimento_silver.parquet/"))
+df = spark.read.parquet("dbfs:/mnt/financeiro/Silver/recebimento_silver.parquet/")
+df_sorted = df.sort(df['DataCarga'].desc())
+
+display(df_sorted)
